270465dcfbe19b1d29bb85d1c0e9f22b3b55c8fd
Olawale Batashi <olaclassik@gmail.com>
2019-11-04 22:43:58 +0100

    refactor(index): remove console codes and comments

--- src/index.js
+++ src/index.js
@@ -2,20 +2,15 @@ let billionsNames = require("./billions-names.json");
 let uniqueRandomArray = require("unique-random-array");
 let getRandomItem = uniqueRandomArray(billionsNames);
 
-// package.json husky settings
-// "pre-push": "npm run test:single"
-
 module.exports = {
   all: billionsNames,
   random: random
 };
 
 function random(number) {
-  console.log(number);
   if (number === undefined) {
     return getRandomItem();
   } else {
-    console.log(number);
     let randomItems = [];
     for (let i = 0; i < number; i++) {
       randomItems.push(getRandomItem());
