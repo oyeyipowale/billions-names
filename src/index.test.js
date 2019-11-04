9f35c4c6fabdca1a98919271e1b163a30327dfcd
Olawale Batashi <olaclassik@gmail.com>
2019-11-04 15:39:35 +0100

    test(test): converted var to let
    more modern javascript syntax
    
    closes #5

--- src/index.test.js
+++ src/index.test.js
@@ -20,12 +20,12 @@ describe("billions-names", function() {
 
   describe("random", function() {
     it("should return a random item from the billions.all", function() {
-      var randomItem = billions.random();
+      let randomItem = billions.random();
       expect(billions.all).to.include(randomItem);
     });
 
     it("should return an array of random items if passed a number", function() {
-      var randomItems = billions.random(3);
+      let randomItems = billions.random(3);
       expect(randomItems).to.have.length(3);
       randomItems.forEach(function(item) {
         expect(billions.all).to.include(item);
